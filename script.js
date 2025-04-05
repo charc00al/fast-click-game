5958d8bac516274869b170f4302c0daeb5182534
charcOOal <sarka-stranska@seznam.cz>
2025-04-05 11:55:03 +0200

    refactor: split game into functions Move button, Keep score, and updating high score. Fix updating HS.

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 var btn = document.querySelector(".btn");
 var score = document.querySelector(".score");
-var counter = 0;
+var highScore = document.querySelector(".highest-score")
 
+var counter = 0;
+var score_counter
+
+// game, listen to click on button
 btn.addEventListener("click", function () {
-    btn.style.left = Math.floor(Math.random() * 600) + "px";
-    btn.style.top = Math.floor(Math.random() * 370) + "px";
-    counter++;
-    score.textContent = "Score: " + counter;
+    moveButton();
+    keepScore();
+    updateHighScore();
 });
 
+// moving the button to random position
+function moveButton() {
+    btn.style.left = Math.floor(Math.random() * 600) + "px";
+    btn.style.top = Math.floor(Math.random() * 370) + "px";
+}
+
+// keeping score 
+function keepScore() {
+    counter++;
+    score.textContent = "Score: " + counter;
+}
+
+// updating high score
+function updateHighScore() {
+    localStorage.setItem("high-score", counter);
+    highScore.textContent = "Highest score: " + localStorage.getItem("high-score") + 1; 
+}
