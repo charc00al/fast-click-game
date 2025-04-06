caf8d406eecbf0e51d021a5bf65b7108b9a86b53
charcOOal <sarka-stranska@seznam.cz>
2025-04-06 10:35:02 +0200

    feat: add 30s timer

--- script.js
+++ script.js
@@ -2,16 +2,42 @@
 var btn = document.querySelector(".btn");
 var scoreText = document.querySelector(".score");
 var highScoreText = document.querySelector(".highest-score")
+var timerText = document.querySelector(".timer")
 
 var score = 0;
 var highScore = 0;
+var seconds = 30;
+
+// create a game start function, that starts with first button-click on the circle? 
+    // or should it have a game start button? -- i think click on the circle triggers start
+// game start triggers countdown 
+// after 30s is game over 
+    // or change timer with levels?
+// what should happen, when the game is over?
+    // -- game over message
+    // -- play again button?
+    // -- reset score?
+
+
+
+// 30 second timer
+const timer = setInterval(tickingSeconds, 1000);
+
+function tickingSeconds() {
+    currentTime = seconds--;
+    timerText.textContent = "Time remaining: " + currentTime + " seconds";
+
+    if (seconds == -1) {
+        clearInterval(timer);
+    }
+}
+
 
 // game, listen to click on button
 btn.addEventListener("click", function () {
     moveButton();
     keepScore();
     updateHighScore();
-
 });
 
 // moving the button to random position
