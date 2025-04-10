ee2601563e51e28ebe4aa570c1e49dbe41a79e7f
charcOOal <sarka-stranska@seznam.cz>
2025-04-10 10:44:25 +0200

    feat: make timer start and reset

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ const highScoreText = document.querySelector(".highest-score")
 const timerText = document.querySelector(".timer")
 const startBtn = document.querySelector(".start-btn")
 
-
+let timer = null;
 
 startBtn.addEventListener("click", function(){
     startTimer();
-
 })
 
 
 function startTimer() {
-    let seconds = 5;
+    if (timer) {
+        clearInterval(timer);
+    }
 
+    let seconds = 10;
 
+    timer = setInterval(tickingSeconds, 1000);
     function tickingSeconds() {
         currentTime = seconds--;
         timerText.textContent = "Time remaining: " + currentTime + " seconds";
@@ -25,7 +28,4 @@ function startTimer() {
             clearInterval(timer);
         }
     }
-
-    const timer = setInterval(tickingSeconds, 1000);
-
 }
